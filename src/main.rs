bd0415afce55646cfbf2efcb3f9ceb10f78050dd
kprkacin <62102043+kprkacin@users.noreply.github.com>
2024-08-30 11:34:24 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ use serde_json::*;
 pub fn get(_req: Request<Vec<u8>>) -> Response<Vec<u8>> {
     // Create a JSON object
     let response_json = json!({
-        "response": "Hello world from K3"
+        "response": "Hello world from K32"
     });
 
     // Convert JSON object to a string and then to bytes
     let response_body = response_json.to_string().into_bytes();
 
+    
     // Build the response
     Response::builder()
         .status(200)
